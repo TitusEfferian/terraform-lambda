f94d65c06008a448b7f535827d5b6b76bf48140d
titusefferian <tituseff@gmail.com>
2024-01-21 22:29:03 +0900

    first commit

---
+++ main.tf
@@ -0,0 +1,59 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.33.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "ap-northeast-1"
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+data "archive_file" "lambda" {
+  type        = "zip"
+  source_file = "index.js"
+  output_path = "lambda_function_payload.zip"
+}
+
+resource "aws_lambda_function" "test_lambda" {
+  filename      = "lambda_function_payload.zip"
+  function_name = "test-terraform-lambda"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "index.handler"
+
+
+  source_code_hash = data.archive_file.lambda.output_base64sha256
+
+  runtime = "nodejs18.x"
+
+  environment {
+    variables = {
+      foo = "bar"
+    }
+  }
+}
+
+
+resource "aws_lambda_function_url" "test_latest" {
+  function_name      = aws_lambda_function.test_lambda.function_name
+  authorization_type = "NONE"
+}
